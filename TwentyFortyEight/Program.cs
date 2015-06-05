25568c2dfc7b1188054a7cff672c447305bbf735
Anton Forbes <anton.forbes@bt.com>
2015-06-05 16:55:33 +0100

    add basic row processing for 2048

--- TwentyFortyEight/Program.cs
+++ TwentyFortyEight/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace TwentyFortyEight
 {
-    class Program
+    public static class GameLogic
     {
         private static int[,] board;
         private static int gridsize;
@@ -44,7 +44,22 @@ namespace TwentyFortyEight
 
         public static int[] processRow(int[] input)
         {
-            return null;
+            var len = input.Length;
+            for(var i = 0; i< input.Length -1; i++)
+            {
+                if(input[i] !=  0 && input[i] == input[i+1])
+                {
+                    input[i + 1] += input[i];
+                    input[i] = 0;
+                }
+            }
+            List<int> result = input.Where(x => x != 0).ToList<int>();
+
+            while (result.Count != input.Length)
+            {
+                result.Insert(0, 0);
+            }
+            return result.ToArray();
         }
 
 
--- TwentyFortyEightTest/UnitTest1.cs
+++ TwentyFortyEightTest/UnitTest1.cs
@@ -1,14 +1,47 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwentyFortyEight;
+
 
 namespace TwentyFortyEightTest
 {
     [TestClass]
-    public class UnitTest1
+    public class Test2048
     {
+        
         [TestMethod]
-        public void TestMethod1()
+        public void TestRowProcessMovesOver0()
         {
+            int[] input = new int[4] { 2, 0, 0, 4 };
+            int[] expected = new int[4] { 0, 0, 2, 4 };
+            CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
         }
+        
+        [TestMethod]
+        public void TestRowProcessAddsUpEquals()
+        {
+            int[] input = new int[4] { 0, 0, 4, 4 };
+            int[] expected = new int[4] { 0, 0, 0, 8};
+            CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
+        }
+
+        [TestMethod]
+        
+        public void TestRowProcessAddsUpEqualsAndMovesOver0()
+        {
+            int[] input = new int[4] { 2, 4, 2, 2 };
+            int[] expected = new int[4] { 0, 2, 4, 4 };
+            CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
+        }
+       
+        [TestMethod]
+        public void TestRowProcessDoesntChangeUnNeeded()
+        {
+            int[] input = new int[4] { 0, 0, 0, 8 };
+            int[] expected = new int[4] { 0, 0, 0, 8 };
+            CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
+        }
+
+
     }
 }
