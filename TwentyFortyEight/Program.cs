f611199148c749b479d354b04cb37c87810d7faa
Antonf26 <antonf26@gmail.com>
2015-06-07 11:48:58 +0100

    Add column logic and output

--- TwentyFortyEight/Program.cs
+++ TwentyFortyEight/Program.cs
@@ -8,7 +8,7 @@ namespace TwentyFortyEight
 {
     public static class GameLogic
     {
-        private static int[,] board;
+        private static int[][] board;
         private static int gridsize;
         private static string direction;
 
@@ -18,27 +18,62 @@ namespace TwentyFortyEight
             string[] fileLines = System.IO.File.ReadAllLines(args[0]);
             foreach (string line in fileLines)
             {
-                processLine(line);
+                Console.WriteLine(processLine(line));
             }
+            Console.ReadLine();
         }
 
-        private static void processLine(string line)
+        public static string processLine(string line)
         {
             string[] values = line.Replace(" ", string.Empty).Split(';');
             direction = values[0];
             gridsize = Convert.ToInt32(values[1]);
-            board = new int[gridsize, gridsize];
-            char[] vals;
+            board = new int[gridsize][];
+            
             string[] gridLines = values[2].Split('|');
 
-            for(var i = 0; i < gridLines.Length; i++)
+            for (var i = 0; i < gridLines.Length; i++)
             {
-                vals = gridLines[i].ToCharArray();
-                for(var j = 0; j < gridLines.Length; j++)
-                {
-                    board[i, j] = (int)Char.GetNumericValue(vals[j]);
-                }
+                board[i] = Array.ConvertAll(gridLines[i].ToCharArray(), ch => (int)Char.GetNumericValue(ch));
             }
+
+            switch (direction)
+            {
+                case "RIGHT":
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        board[i] = processRow(board[i]);
+                    }
+                    break;
+                case "LEFT":
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        board[i].Reverse();
+                        board[i] = processRow(board[i]);
+                        board[i].Reverse();
+                    }
+                    break;
+                case "DOWN":
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        writeColumn(processRow(getColumn(i)), i);
+                    }
+                    break;
+                case "UP":
+                    for (var i = 0; i < board.Length; i++)
+                    {
+                        int[] col = getColumn(i);
+                        col.Reverse();
+                        col = processRow(col);
+                        col.Reverse();
+                        writeColumn(col, i);
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return string.Join("|", board.Select(
+                row => string.Join(" ", Array.ConvertAll(row, num => num.ToString()))).ToArray());
         }
 
 
@@ -46,9 +81,9 @@ namespace TwentyFortyEight
         {
             var len = input.Length;
             input = input.Where(x => x != 0).ToArray();
-            for(var i = 0; i< input.Length -1; i++)
+            for (var i = 0; i < input.Length - 1; i++)
             {
-                if(input[i] !=  0 && input[i] == input[i+1])
+                if (input[i] != 0 && input[i] == input[i + 1])
                 {
                     input[i + 1] += input[i];
                     input[i] = 0;
@@ -66,8 +101,19 @@ namespace TwentyFortyEight
         public static int[] getColumn(int index)
         {
             int[] res = new int[gridsize];
-
+            for (var i = 0; i < res.Length; i++)
+            {
+                res[i] = board[i][index];
+            }
             return res;
+        }
+
+        public static void writeColumn(int[] column, int index)
+        {
+            for (var i = 0; i < column.Length; i++)
+            {
+                board[i][index] = column[i];
+            }
         }
 
 
--- TwentyFortyEightTest/RowLogicTest.cs
+++ TwentyFortyEightTest/RowLogicTest.cs
@@ -8,39 +8,7 @@ namespace TwentyFortyEightTest
     public class Test2048
     {
         
-        //[TestMethod]
-        //public void TestRowProcessMovesOver0()
-        //{
-        //    int[] input = new int[4] { 2, 0, 0, 4 };
-        //    int[] expected = new int[4] { 0, 0, 2, 4 };
-        //    Microsoft.VisualStudio.TestTools.UnitTesting.CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
-        //}
-        
-        //[TestMethod]
-        //public void TestRowProcessAddsUpEquals()
-        //{
-        //    int[] input = new int[4] { 0, 0, 4, 4 };
-        //    int[] expected = new int[4] { 0, 0, 0, 8};
-        //    Microsoft.VisualStudio.TestTools.UnitTesting.CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
-        //}
-
-        //[TestMethod]
-        
-        //public void TestRowProcessAddsUpEqualsAndMovesOver0()
-        //{
-        //    int[] input = new int[4] { 2, 4, 2, 2 };
-        //    int[] expected = new int[4] { 0, 2, 4, 4 };
-        //    Microsoft.VisualStudio.TestTools.UnitTesting.CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
-        //}
        
-        //[TestMethod]
-        //public void TestRowProcessDoesntChangeUnNeeded()
-        //{
-        //    int[] input = new int[4] { 0, 0, 0, 8 };
-        //    int[] expected = new int[4] { 0, 0, 0, 8 };
-        //    Microsoft.VisualStudio.TestTools.UnitTesting.CollectionAssert.AreEqual(GameLogic.processRow(input), expected);
-        //}
-
         [TestCase (new int[] {0,0,4,4}, new int[] {0,0,0,8}, "Should add up adjacent same numbers", TestName="adds adjacent same")]
         [TestCase(new int[] { 0, 4, 0, 4 }, new int[] { 0, 0, 0, 8 }, "Should add up same numbers over 0", TestName="adds same over empty")]
         [TestCase(new int[] { 2, 0, 0, 4 }, new int[] { 0, 0, 2, 4 }, "Should move over 0", TestName="Moves over empty")]
